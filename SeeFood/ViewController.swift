37267fe51085981611d9be62c89a89fb19b06112
Kenin Page <kenin.page@kenins-macbook-pro.local>
2018-02-11 13:17:22 -0500

    Initial save, accessing camera

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera  //when using on simulator you will need to use the .photoLobrary option instead of .camera
+        imagePicker.allowsEditing = false //if you want to crop then you may want to allow editing
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImpage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImpage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
-
-
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present (imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
